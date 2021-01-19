1f89c2681a86cac5cfdfcb21914fd2a696b675de
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2021-01-19 23:58:55 +0700

    create component Portfolio

---
+++ components/Portfolio.js
@@ -0,0 +1,12 @@
+const Portfolio = () => {
+  return (
+    <div className="container">
+      <div className="tw-flex tw-flex-col tw-justify-center tw-items-center">
+        <h1 className="tw-font-medium fs-36">Portfolio</h1>
+        <p className="fs-16 mt-4">Check out my portfolio to see my recent work</p>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import About from "components/About"
 import Header from "components/Header"
+import Portfolio from "components/Portfolio"
 
 const Home = () => {
   return (
     <div>
       <Header />
       <About />
+      <Portfolio />
     </div>
   )
 }
