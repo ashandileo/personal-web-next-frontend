d303224d584b8704686e3a4eb838cb8f17c2eea8
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2021-01-28 21:19:18 +0700

    slicing skills

--- components/admin/Skills.js
+++ components/admin/Skills.js
@@ -1,6 +1,49 @@
+import { Button, Image, Table, Tag } from 'antd';
+
 const Skills = () => {
+
+  const columns = [
+    {
+      title: "Skill Name",
+      dataIndex: "skill",
+      key: "skill",
+      render: text => text
+    },
+    {
+      title: "Picture",
+      dataIndex: "picture",
+      key: "picture",
+      render: image => (
+        <Image
+          width={100}
+          src={image}
+        />
+      )
+    }
+  ]
+
+  const data = [
+    {
+      key: 1,
+      skill: "React JS",
+      picture: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+    },
+  ]
+
   return (
-    <div>Skills</div>
+    <div>
+      <div className="tw-flex tw-justify-between tw-items-center">
+        <h1 className="fs-24 tw-font-normal">Skills</h1>
+        <Button type="primary">Create Skills</Button>
+      </div>
+      <div className="mt-20">
+        <Table
+          columns={columns}
+          dataSource={data}
+          pagination={false}
+        />
+      </div>
+    </div>
   )
 }
 
