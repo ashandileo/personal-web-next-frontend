993a9e66f15a3890744a377304db0d351bc9642a
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2021-01-29 22:05:19 +0700

    handle post skill

--- components/shared/Modal/ModalCreateEditSkill.js
+++ components/shared/Modal/ModalCreateEditSkill.js
@@ -1,32 +1,56 @@
 import { useState } from "react"
-import { Modal, Form, Input, Select, Upload, Button } from 'antd';
+import { Modal, Form, Input, message, Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import { postSkill } from "client/SkillClient";
 
 const ModalCreateEditSkill = ({
-  onClickOk,
   onClickCancel,
   visible
 }) => {
 
+  const [name, setName] = useState("")
+  const [fileUploaded, setFileUploaded] = useState("")
+
+  const handleChangeUpload = e => {
+    if (e.file.response) {
+      setFileUploaded(e.file.response.url)
+    }
+  }
+
+  const handleSubmitModal = async () => {
+    const payload = {
+      name: name,
+      picture: fileUploaded
+    }
+
+    const { data } = await postSkill(payload)
+    if (data) {
+      message.success(`Berhasil menambahkan data`)
+      onClickCancel()
+    }
+  } 
+
   return (
     <Modal
-      title="Create Portfolio"
+      title="Add Skill"
       centered
       visible={visible}
-      onOk={onClickOk}
+      onOk={handleSubmitModal}
       onCancel={onClickCancel}
       width={1000}
       bodyStyle={{ maxHeight: "500px", overflow: "auto" }}
     >
-      <Form layout="vertical">
-        <Form.Item label="Title">
-          <Input placeholder="Title" />
+      <Form layout="vertical" onFinish={handleSubmitModal}>
+        <Form.Item label="Name">
+          <Input placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
         </Form.Item>
         <Form.Item label="Picture">
           <Upload
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            action="http://localhost:3333/upload"
+            name="image"
             listType="picture"
-            defaultFileList={[]}
+            onChange={e => handleChangeUpload(e)}
+            maxCount={1}
           >
             <Button icon={<UploadOutlined />}>Upload</Button>
           </Upload>
